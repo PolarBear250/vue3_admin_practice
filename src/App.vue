b45a3eee62be0411c7bab5b7bc35e3070d36cd67
OrangeCat007 <13101742+orangecat007@user.noreply.gitee.com>
2023-07-02 16:27:34 +0800

    提交没有格式化的代码

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>我是APP</h2>
+  </div>
+</template>
+
+<script setup lang='ts'>
+const fun = () =>
+{
+  console.log('这是没有格式化的代码');
+  
+}
+</script>
+
+<style>
+
+</style>
+
